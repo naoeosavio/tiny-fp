1bdcabe7754d5a080b1a5449f8a959b5dc3b8aea
nao e o savio <savioscmendes@gmail.com>
2025-10-16 19:36:17 -0300

    refactor: update index exports for new module structure
    - Reorganize exports to use individual function exports
    - Export namespace aliases (OptionNS, EitherNS, etc.) for compatibility
    - Maintain type exports for all data types
    - Update import structure to support tree-shaking while keeping namespace support

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,19 @@
-export * from "./Option";
-export * from "./Either";
-export * from "./Result";
-export * from "./Pair";
-export * from "./Triple";
+// Either
+export type { Either, Left, Right } from "./Either";
+export { EitherNS, left, right } from "./Either";
+
+// Option
+export type { None, Option, Some } from "./Option";
+export { none, OptionNS, some } from "./Option";
+
+// Pair
+export type { Pair } from "./Pair";
+export { PairNS, pair } from "./Pair";
+
+// Result
+export type { Done, Fail, Result } from "./Result";
+export { done, fail, ResultNS } from "./Result";
+
+// Triple
+export type { Triple } from "./Triple";
+export { TripleNS, triple } from "./Triple";
